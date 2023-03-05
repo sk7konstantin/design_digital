4bfff0690ad2a73e153c3bb0cad9beef597ceadf
Konstantin <sk7konstantin@yandex.ru>
2023-03-05 19:22:49 +0300

    Реализовал меню-бургер

---
+++ src/js/menu.js
@@ -0,0 +1,16 @@
+const navigaton = document.querySelector('.navigation');
+const button = document.querySelector('.navigation__button-menu');
+const menuList = document.querySelector('.navigation__list');
+const items = document.querySelectorAll('.navigation__item');
+const contactsMobile = document.querySelector('.header__contacts-mobile');
+
+button.addEventListener('click', ()=> {
+    navigaton.classList.toggle('navigation--mobile-bg-on');
+    menuList.classList.toggle('navigation__list--active');
+    contactsMobile.classList.toggle('header__contacts-mobile--active')
+    button.classList.toggle('navigation__button-menu--active')
+
+    for (const i of items) {
+        i.classList.toggle('navigation__item--active')
+    }
+})
